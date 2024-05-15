6fd37def84308a1d51c3735e13e36f0c1975de41
salomoncloud <158000174+salomoncloud@users.noreply.github.com>
2024-05-15 08:29:00 -0400

    Create azure_lb_hw.tf

---
+++ azure_lb_hw.tf
@@ -0,0 +1,35 @@
+locals{
+  loadbalancerconfig=[for f in fileset("${path.module}/loadbalancer", "[^_]*.yaml") : yamldecode(file("${path.module}/loadbalancer/${f}"))]
+  loadbalancerlist = flatten([
+    for app in local.loadbalancerconfig : [
+      for loadbalancer in try(app.loadbalancerconfiguration, []) :{
+        name=loadbalancer.publicipname
+        allocation_method=loadbalancer.allocation_method
+
+      }
+    ]
+])
+}
+resource "azurerm_resource_group" "loadbalancerrg" {
+  name     = "LoadBalancerRG"
+  location = "West Europe"
+}
+
+resource "azurerm_public_ip" "azurepublicipexample" {
+  for_each            = {for publicip in local.loadbalancerlist: "${publicip.name}"=>publicip }
+  name                = each.value.name
+  location            = azurerm_resource_group.loadbalancerrg.location
+  resource_group_name = azurerm_resource_group.loadbalancerrg.name
+  allocation_method   = each.value.allocation_method
+}
+
+resource "azurerm_lb" "azureloadbalancerexample" {
+  for_each            = azurerm_public_ip.azurepublicipexample
+  name                = each.value.name
+  location            = azurerm_resource_group.loadbalancerrg.location
+  resource_group_name = azurerm_resource_group.loadbalancerrg.name
+  frontend_ip_configuration {
+    name                 = "PublicIPAddress"
+    public_ip_address_id = each.value.id
+  }
+}
