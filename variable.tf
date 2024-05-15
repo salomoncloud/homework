7183eeb8921cba0190cb88ea4ad7e6021aae7d28
salomoncloud <158000174+salomoncloud@users.noreply.github.com>
2024-05-15 07:32:58 -0400

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,52 @@
+variable "subscription_id"{
+  type=string
+}
+
+variable "client_id"{
+  type=string
+}
+variable "client_secret"{
+  type=string
+}
+variable "tenant_id"{
+  type=string
+}
+variable "countNumber"{
+  type=number
+  default=0
+}
+variable "account_tier"{
+  type=string
+  default="Standard"
+}
+variable "myname"{
+  type=string
+}
+variable "account_replication_type"{
+  type=string
+  default="GRS"
+}
+variable "access_tier"{
+  type=string
+  default="Cool"
+}
+variable "cross_tenant_replication_enabled"{
+ type=bool
+ default=false
+}
+variable "prefix"{
+ type=string
+ default="mcit"
+}
+variable "component" {
+  type    = list
+  default = ["bastion", "frontproxy", "db", "infra"]
+}
+variable "environment"{
+ type=string
+ default="staging"
+}
+variable "summersports"{
+    type =list(string)
+    default=["soccer","basketball","badminton","tabletennis"]
+}
